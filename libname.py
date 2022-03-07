33f771280332839b56acf3e1d45428530fe4b600
Danilo <76904889+danilofiumi@users.noreply.github.com>
2022-03-07 01:23:12 +0100

    Update libname.py

--- libname.py
+++ libname.py
@@ -1,9 +1,6 @@
 def libname(path):
     import pandas as pd
     import os
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
 
     la_table=os.listdir(path)
     sep = '.'
