3e5f9601d15a889177341b0f1685b9c3dad4bbd9
Danilo <76904889+danilofiumi@users.noreply.github.com>
2022-03-07 01:22:16 +0100

    Create clear_libname.py

---
+++ clear_libname.py
@@ -0,0 +1,12 @@
+def clear_libname(path):
+    import pandas as pd
+    import os
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+    la_table=os.listdir(path)
+    sep = '.'
+
+    for table in la_table:
+        if table.partition(sep)[2] == 'sas7bdat':
+            del globals()[table.split(sep, 1)[0]]
