2d357cfc8883e11e269d4342b01d3b6e48c4fad8
Danilo <76904889+danilofiumi@users.noreply.github.com>
2022-03-07 01:24:58 +0100

    Create export_libname.py

---
+++ export_libname.py
@@ -0,0 +1,29 @@
+def export_libname(path,folder,filename):
+    import pandas as pd
+    import os
+
+    libname(path)
+
+    sep = '.'
+
+    la_table = os.listdir(path)
+
+    dataset=[]
+    for table in la_table:
+
+        if table.partition(sep)[2] == 'sas7bdat':
+            dataset.append(table.split(sep, 1)[0])
+
+    '[%s]' % ', '.join(map(str, dataset))
+
+    Excelwriter = pd.ExcelWriter(folder+"/"+filename+".xlsx",engine="xlsxwriter")
+
+    
+    for i, df in enumerate (dataset):
+        print(type(eval(df)))
+        try:
+            eval(df).to_excel(Excelwriter, sheet_name=str(dataset[i]),index=False)
+        except AttributeError:
+            pass
+
+    Excelwriter.save()
